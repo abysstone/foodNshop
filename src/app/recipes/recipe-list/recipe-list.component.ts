ae5c7e4b16b3864278db877f98c9d939ce973262
abysstone <manasbhatt007@gmail.com>
2021-11-25 12:43:21 +0530

    basic structure

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('Test recipe', 'very Tasty Recipe', 'https://p1.pxfuel.com/preview/843/619/707/borsch-food-cook-at-home-a-simple-recipe-simple-and-delicious-soup.jpg'),
+    new Recipe('Test2 recipe', 'very2 Tasty Recipe', 'https://p1.pxfuel.com/preview/843/619/707/borsch-food-cook-at-home-a-simple-recipe-simple-and-delicious-soup.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
