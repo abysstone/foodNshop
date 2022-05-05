e4407e31121a1157e2de08793f7dbdf1555a6bfc
abysstone <manasbhatt007@gmail.com>
2022-05-05 17:28:53 +0530

    first commit after cloning.. test commit

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,8 +7,8 @@ export class RecipeService{
     recipeSelected= new EventEmitter<Recipe>();
 
     private recipes : Recipe[] = [
-        new Recipe('Test recipe', 'very Tasty Recipe', 'https://p1.pxfuel.com/preview/843/619/707/borsch-food-cook-at-home-a-simple-recipe-simple-and-delicious-soup.jpg'),
-        new Recipe('Test2 recipe', 'very2 Tasty Recipe', 'https://p1.pxfuel.com/preview/843/619/707/borsch-food-cook-at-home-a-simple-recipe-simple-and-delicious-soup.jpg')
+        new Recipe('chinese recipe', 'very Tasty Recipe', 'https://p1.pxfuel.com/preview/843/619/707/borsch-food-cook-at-home-a-simple-recipe-simple-and-delicious-soup.jpg'),
+        new Recipe('Bhindi recipe', 'bhindi ki sabzi', 'https://swadkechatkare.com/wp-content/uploads/2019/08/moongfali-bhindi-sabji.jpg')
       ];
 
     getRecipes(){
